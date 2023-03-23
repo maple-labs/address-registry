c6408c583b5bc675e1a67b03145310b5c538edfc
JG <jg@jgcarv.com>
2023-03-23 13:34:06 -0300

    fix: rename contract

--- contracts/MapleAddressRegistry.sol
+++ contracts/MapleAddressRegistry.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.7;
 
-contract FixedAddresses {
+contract AddressREgistry {
 
     /**************************************************************************************************************************************/
     /*** Multisigs / EOAs                                                                                                               ***/
