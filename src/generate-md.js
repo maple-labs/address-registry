a88cfb100a1985c942e03fdbea9917ee8e55a019
JG <jg@jgcarv.com>
2024-03-05 14:12:08 -0300

    fix: PR comments

--- src/generate-md.js
+++ src/generate-md.js
@@ -11,7 +11,7 @@ const section = (title, depth = 1) => `${'#'.repeat(depth)} ${title}`;
 
 const tableHeader = `| Contract | Address |\n| --- | --- |`;
 
-const declaration = (key, value) => `| ${key} | [${value}](https://etherscan.io/address/${value}) |`;
+const declaration = (key, value, chainName) => `| ${key} | [${value}](https://${chainName == "ETH" ? "etherscan" : "basescan"}.io/address/${value}) |`;
 
 const capitalize = (s) => s.charAt(0).toUpperCase() + s.slice(1);
 
@@ -21,71 +21,72 @@ registry.chains.forEach(chain => {
     let content = [];
     
     for (const [key, value] of Object.entries(chain.contracts)) {  
-        
-        if (key == "pools") {
-            content.push(section(capitalize(key)));
-            for (const [k, v] of Object.entries(value)) {
-                const {name, address, label, ...extra} = v;
-                
-                // Push the content label
-                content.push(section(v.label, 2));
-                content.push(tableHeader);
 
-                // Push the pool declaration e.g aqruPool
-                content.push(declaration(v.name + "Pool", v.address))
-                
-                // Push all other pool relater contracts, always prepending the pool name.
-                if (Object.keys(extra).length > 0) {
-                    for (const [k2, v2] of Object.entries(extra)) {
-                        content.push(declaration(capitalize(k2), v2))
-                    }
-                    content.push('');
-                }
-            }
-
-        } 
-        else if (key == "proxies") {
-            content.push(section(capitalize(key)));
-            for (const [k, v] of Object.entries(value)) {
-                const {name, address, label, ...extra} = v;
-                
-                // Push the content label
-                content.push(section(v.name, 2));
-                content.push(tableHeader);
-                                
-                // Push all other pool relater contracts, always prepending the pool name.
-                if (Object.keys(extra).length > 0) {
-                    for (const [k2, v2] of Object.entries(extra)) {
-                        content.push(declaration(capitalize(k2), v2))
-                    }
-                    content.push('');
-                }
-            }
-        }
-        
-        else {
-            content.push(section(capitalize(key)));
-            content.push(tableHeader);
-
-
-            for (const [k, v] of Object.entries(value)) {
-                const {name, address, ...extra} = v;
-                
-                content.push(declaration(v.name, v.address))
-                
-                if (Object.keys(extra).length > 0) {
+        switch (key) {
+            case "pools":
+                content.push(section(capitalize(key)));
+                for (const [k, v] of Object.entries(value)) {
+                    const {name, address, label, ...extra} = v;
                     
-                    for (const [k2, v2] of Object.entries(extra)) {
-                        if (key == "tokens") {
-                            content.push(declaration(v.name + capitalize(k2), v2))
-                        } else {
-                            content.push(declaration(capitalize(k2), v2))
+                    // Push the content label
+                    content.push(section(v.label, 2));
+                    content.push(tableHeader);
+        
+                    // Push the pool declaration e.g aqruPool
+                    content.push(declaration(v.name + "Pool", v.address, chain.name))
+                    
+                    // Push all other pool relater contracts, always prepending the pool name.
+                    if (Object.keys(extra).length > 0) {
+                        for (const [k2, v2] of Object.entries(extra)) {
+                            content.push(declaration(capitalize(k2), v2, chain.name))
+                        }
+                        content.push('');
+                    }
+                }
+                break;
+            case "proxies":
+                content.push(section(capitalize(key)));
+                for (const [k, v] of Object.entries(value)) {
+                    const {name, address, label, ...extra} = v;
+                    
+                    // Push the content label
+                    content.push(section(v.name, 2));
+                    content.push(tableHeader);
+                                    
+                    // Push all other pool relater contracts, always prepending the pool name.
+                    if (Object.keys(extra).length > 0) {
+                        for (const [k2, v2] of Object.entries(extra)) {
+                            content.push(declaration(capitalize(k2), v2, chain.name))
+                        }
+                        content.push('');
+                    }
+                }
+                break;
+        
+            default:
+                content.push(section(capitalize(key)));
+                content.push(tableHeader);
+        
+                for (const [k, v] of Object.entries(value)) {
+                    const {name, address, ...extra} = v;
+                    
+                    content.push(declaration(v.name, v.address))
+                    
+                    if (Object.keys(extra).length > 0) {
+                        
+                        for (const [k2, v2] of Object.entries(extra)) {
+                            if (key == "tokens") {
+                                content.push(declaration(v.name + capitalize(k2), v2, chain.name))
+                            } else {
+                                content.push(declaration(capitalize(k2), v2, chain.name))
+                            }
                         }
                     }
+        
                 }
-
-            }
+                break;
         }
+
     }
 
     writeFile(`./MapleAddressRegistry${chain.name}.md`, content.join('\n'), (err) => {
--- src/generate-solidity.js
+++ src/generate-solidity.js
@@ -9,7 +9,7 @@ const registry = await importJson("../address-registry.json");
 
 const header = `// SPDX-License-Identifier: BUSL-1.1 \npragma solidity ^0.8.7; \n\n// WARNING: File generated automatically, do not edit manually`;
 
-const title = (name) => `contract MapleAddressRegistry${name} {`
+const title = (name) => `contract MapleAddressRegistry${name} {\n`;
 
 const section = (title) => `    /${'*'.repeat(134)}/\n    /*** ${title}${' '.repeat(134 - title.length - 7)}***/\n    /${'*'.repeat(134)}/\n`;
 
@@ -26,58 +26,57 @@ registry.chains.forEach(chain => {
     
     for (const [key, value] of Object.entries(chain.contracts)) {
         
-        // Handling Pool sections
-        if (key == "pools") {
-            for (const [k, v] of Object.entries(value)) {
-                const {name, address, label, ...extra} = v;
-                
-                const longestKeyLength = longestKey(extra).length;
-
-                // Push the content label
-                content.push(section(v.label));
-                
-                // Push the pool declaration e.g aqruPool
-                content.push(declaration(v.name + "Pool", v.address, longestKeyLength - 4,))
-                
-                // Push all other pool relater contracts, always prepending the pool name.
-                if (Object.keys(extra).length > 0) {
-                    for (const [k2, v2] of Object.entries(extra)) {
-                        content.push(declaration(capitalize(k2), v2, longestKeyLength - k2.length, v.name))
-                    }
-                    content.push('');
-                }
-            }
-
-        } else {
-            content.push(section(capitalize(key)));
-
-            for (const [k, v] of Object.entries(value)) {
-                const longestNameLength = longestName(value).name.length;
-                const {name, address, ...extra} = v;
-                
-                content.push(declaration(v.name, v.address, key == "tokens" ? 6 : longestNameLength - v.name.length))
-                
-                if (Object.keys(extra).length > 0) {
+        switch (key) {
+            case "pools":
+                for (const [k, v] of Object.entries(value)) {
+                    const {name, address, label, ...extra} = v;
                     
-                    if (key == "proxies") {
-                        content.pop();
-                    }
-
                     const longestKeyLength = longestKey(extra).length;
-
-                    for (const [k2, v2] of Object.entries(extra)) {
-                        content.push(declaration(capitalize(k2), v2, longestKeyLength - k2.length, v.name))
+        
+                    // Push the content label
+                    content.push(section(v.label));
+                    
+                    // Push the pool declaration e.g aqruPool
+                    content.push(declaration(v.name + "Pool", v.address, longestKeyLength - 4,))
+                    
+                    // Push all other pool relater contracts, always prepending the pool name.
+                    if (Object.keys(extra).length > 0) {
+                        for (const [k2, v2] of Object.entries(extra)) {
+                            content.push(declaration(capitalize(k2), v2, longestKeyLength - k2.length, v.name))
+                        }
+                        content.push('');
                     }
-                    content.push('');
                 }
-
-            }
+                break;
+            default:
+                content.push(section(capitalize(key)));
+                for (const [k, v] of Object.entries(value)) {
+                    const longestNameLength = longestName(value).name.length;
+                    const {name, address, ...extra} = v;
+                    
+                    content.push(declaration(v.name, v.address, key == "tokens" ? 6 : longestNameLength - v.name.length))
+                    
+                    if (Object.keys(extra).length > 0) {
+                        
+                        if (key == "proxies") {
+                            content.pop();
+                        }
+        
+                        const longestKeyLength = longestKey(extra).length;
+        
+                        for (const [k2, v2] of Object.entries(extra)) {
+                            content.push(declaration(capitalize(k2), v2, longestKeyLength - k2.length, v.name))
+                        }
+                        content.push('');
+                    }
+        
+                }
         }
         content.push('');
     }
     
-    content.push(`}`);
-    
+    content.push(`}\n`);
+
     writeFile(`./contracts/MapleAddressRegistry${chain.name}.sol`, content.join('\n'), (err) => {
         if (err) throw err;
         console.log('The file has been saved!');
