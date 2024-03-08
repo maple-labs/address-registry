51ad62186eb936687b8fa908ceddf7a78bbce67c
Joao Gabriel Carvalho <jg@jgcarv.com>
2024-03-08 16:31:26 -0300

    test: dependapon trigger (#22)

--- contracts/MapleAddressRegistry.sol
+++ contracts/MapleAddressRegistry.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.7;
 
+// TODO: temporary comment to test dependabot on parent submodules
+
 contract MapleAddressRegistry {
 
     /**************************************************************************************************************************************/
