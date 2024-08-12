4d8ae15d05795886de6fb7a70c2cb25304e07715
JG Carvalho <jg@jgcarv.com>
2024-08-12 15:09:40 -0300

    chore: Move router and add USDT router

--- contracts/MapleAddressRegistryETH.sol
+++ contracts/MapleAddressRegistryETH.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.7;
+// SPDX-License-Identifier: BUSL-1.1 
+pragma solidity ^0.8.7; 
 
 // WARNING: File generated automatically, do not edit manually
 contract MapleAddressRegistryETH {
@@ -104,6 +104,7 @@ contract MapleAddressRegistryETH {
     address constant public syrupUSDCOpenTermLoanManager    = 0x6ACEb4cAbA81Fa6a8065059f3A944fb066A10fAc;
     address constant public syrupUSDCWithdrawalManagerQueue = 0x1bc47a0Dd0FdaB96E9eF982fdf1F34DC6207cfE3;
     address constant public syrupUSDCPoolDelegateCover      = 0x9e62FE15d0E99cE2b30CE0D256e9Ab7b6893AfF5;
+    address constant public syrupUSDCSyrupRouter            = 0x134cCaaA4F1e4552eC8aEcb9E4A2360dDcF8df76;
 
     /**************************************************************************************************************************************/
     /*** Syrup USDT                                                                                                                     ***/
@@ -115,6 +116,7 @@ contract MapleAddressRegistryETH {
     address constant public syrupUSDTOpenTermLoanManager    = 0x616022E54324eF9c13B99c229Dac8ea69AF4FAFf;
     address constant public syrupUSDTWithdrawalManagerQueue = 0x86eBDf902d800F2a82038290B6DBb2A5eE29eB8C;
     address constant public syrupUSDTPoolDelegateCover      = 0x610d99d86d48b385b2ed17a0063e53B5c98E15A1;
+    address constant public syrupUSDTSyrupRouter            = 0xf007476bb27430795138c511f18f821e8d1e5ee2;
 
 
     /**************************************************************************************************************************************/
@@ -175,6 +177,7 @@ contract MapleAddressRegistryETH {
 
     address constant public globalsV3Implementation = 0x5A64417823E8382a7e8957E4411873FE758E73a8;
 
+
     /**************************************************************************************************************************************/
     /*** Singletons                                                                                                                     ***/
     /**************************************************************************************************************************************/
@@ -192,7 +195,6 @@ contract MapleAddressRegistryETH {
     address constant public poolPermissionManager = 0xBe10aDcE8B6E3E02Db384E7FaDA5395DD113D8b3;
     address constant public protocolHealthChecker = 0xfFfFcbA2234b01e7324158EFb83F2f3ED82d1B0D;
     address constant public xmplHealthChecker     = 0xF1714088AA71aedAA8EbdaB7A06cAb0748b84Df8;
-    address constant public syrupRouter           = 0x134cCaaA4F1e4552eC8aEcb9E4A2360dDcF8df76;
     address constant public syrupRateProvider     = 0xd2C59781F1Db84080A0592CE83Fe265642A4a8Eb;
 
     /**************************************************************************************************************************************/
@@ -208,8 +210,8 @@ contract MapleAddressRegistryETH {
     address constant public wbtc          = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
     address constant public wbtcUsdOracle = 0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c;
 
-    address constant public mplv1 = 0x33349B282065b0284d756F0577FB39c158F935e6;
-    address constant public xmpl  = 0x4937A209D4cDbD3ecD48857277cfd4dA4D82914c;
+    address constant public mplv1          = 0x33349B282065b0284d756F0577FB39c158F935e6;
+    address constant public xmpl          = 0x4937A209D4cDbD3ecD48857277cfd4dA4D82914c;
 
     /**************************************************************************************************************************************/
     /*** Mplv2                                                                                                                          ***/
@@ -220,5 +222,7 @@ contract MapleAddressRegistryETH {
     address constant public mplV2Initializer            = 0x7f3C3636208A18c7941BF051807db56864061465;
     address constant public mplV2Migrator               = 0x7b0267C13B994cdb58b8ED3a65b7A09a07432A76;
     address constant public mplV2RecapitalizationModule = 0x7D75cF9Aab6cB9598bB6d9Bd81BaAA288cecA9Bf;
+
+
 
 }
