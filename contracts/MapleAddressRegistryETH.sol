e86cb84018300262c65774c7904dc1b6ae0c301a
Danilo Kitanovic <116795362+kitanovicd@users.noreply.github.com>
2026-02-16 15:52:12 +0100

    feat: Update `ProtocolHealthChecker` address (#52)
    * feat: update ProtocolHealthChecker address
    
    * fix: remove new lines

--- contracts/MapleAddressRegistryETH.sol
+++ contracts/MapleAddressRegistryETH.sol
@@ -181,7 +181,7 @@ contract MapleAddressRegistryETH {
     address constant public fixedTermRefinancerV2 = 0x27ea6e67FB62AB2A603d4ACBc9377D7a9A0fd5e3;
     address constant public openTermRefinancer    = 0x653D4947620B73a433cAbBc9DFb068c3e9c18984;
     address constant public poolPermissionManager = 0xBe10aDcE8B6E3E02Db384E7FaDA5395DD113D8b3;
-    address constant public protocolHealthChecker = 0x0fc32214f7B5F8b9F8B4fFCE8B72E0c4D30cEa79;
+    address constant public protocolHealthChecker = 0xc70f1DB57f72055c91cc35542aeaf9045419D67F;
     address constant public xmplHealthChecker     = 0xF1714088AA71aedAA8EbdaB7A06cAb0748b84Df8;
     address constant public syrupRateProvider     = 0xd2C59781F1Db84080A0592CE83Fe265642A4a8Eb;
     address constant public mplUserActions        = 0x75B7B44dCD28df0beda913Ae2809ede986e8F461;
@@ -216,7 +216,6 @@ contract MapleAddressRegistryETH {
     address constant public syrupRecapitalizationModule = 0x5dfe0460f66fa06bFCbB3211e723556be6B3f69D;
     address constant public syrupStSyrup                = 0xc7E8b36E0766D9B04c93De68A9D47dD11f260B45;
     address constant public syrupSyrupOftAdapter        = 0x688AEe022AA544f150678B8E5720b6b96a9E9a2F;
-
 
     /**************************************************************************************************************************************/
     /*** MapleCCIPReceiver                                                                                                              ***/
